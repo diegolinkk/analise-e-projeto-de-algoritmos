039eb96ce55e22693ed87ea5fc0d2ec353ecebb0
Diego Oliveira <diegolinkk@gmail.com>
2023-08-16 23:17:25 -0300

    ajustes insertion sort

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -1,17 +1,16 @@
 def insertion_sort(lista):
     print(lista)
     tamanho_lista = len(lista)
-    tamanho_lista
     for i in range(tamanho_lista):
-        print(i)
-        # for j in range(i + 1,tamanho_lista):
-        #     if(lista[j] < lista[i]):
-        #         lista[i],lista[j] = lista[j], lista[i]
-        #     print(f'i = {i} j = {j}')
+        #aqui ele não percorre o ultimo laço pq o valor de j ultrapassa o valor do tamanho da lista
+        for j in range(i + 1,tamanho_lista): 
+            if(lista[j] < lista[i]):
+                lista[i],lista[j] = lista[j], lista[i]
+            print(f'i = {i} j = {j}')
     
     print(lista)
 
 
 
-lista = [3,2,1]
+lista = [3,2,1,14,77,90123,3,45,6,25,3,4,1]
 insertion_sort(lista)
