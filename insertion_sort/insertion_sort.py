a08f181e0012b21315a613c2cc6f7a53160df730
Diego Oliveira <diegolinkk@gmail.com>
2023-08-16 23:11:10 -0300

    implantado insertion sort

---
+++ insertion_sort/insertion_sort.py
@@ -0,0 +1,17 @@
+def insertion_sort(lista):
+    print(lista)
+    tamanho_lista = len(lista)
+    tamanho_lista
+    for i in range(tamanho_lista):
+        print(i)
+        # for j in range(i + 1,tamanho_lista):
+        #     if(lista[j] < lista[i]):
+        #         lista[i],lista[j] = lista[j], lista[i]
+        #     print(f'i = {i} j = {j}')
+    
+    print(lista)
+
+
+
+lista = [3,2,1]
+insertion_sort(lista)
